31b7c6c12a0c6b3711e444fbe040b1480b52613f
lui154 <75495661+lui154@users.noreply.github.com>
2021-07-23 10:36:36 -0300

    Add files via upload

---
+++ 23.07.2021 - Exercício herança  polimorfismo/3 - Bebida/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Alcoolica a = new Alcoolica("Skol", "Skol Beats", 4.5);
+        Refrigerante r = new Refrigerante("Gasosa Paranaense", "Paranaense", "Guaraná");
+        Suco s = new Suco("Del Valle", "Del Valle Origens", "Uva");
+
+    }
+}
